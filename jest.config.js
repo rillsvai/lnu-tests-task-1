bb7bb532071153ad029be606edc951301b263c7a
RillSvai <kirilkvas07@gmail.com>
2024-09-07 08:05:48 +0300

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,16 @@ export default {
     "^.+\\.jsx?$": "babel-jest",
   },
   testEnvironment: "node",
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}", "!src/**/*.d.ts"],
+  coverageDirectory: "coverage",
+  coverageReporters: ["json", "lcov", "text", "clover"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
